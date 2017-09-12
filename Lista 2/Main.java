7eb34bb26bcd7645624bf1cb81b51544f019ea7a
lucaszsd <lzsd@cin.ufpe.br>
2017-09-11 22:00:35 -0300

    Add files via upload

--- Lista 2/Main.java
+++ Lista 2/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
 		Scanner in = new Scanner(System.in);
 		
+		int contador = 0;
+		
 		int k = 0;
 		int m = 0;
 		int n = 0;
@@ -20,64 +22,91 @@ public class Main {
 		
 		int aux = 0;
 		int cont = 0;
-	
-		//----------------------------------
 		
-		k = in.nextInt();
-		m = in.nextInt();
-		n = in.nextInt();
-		q = in.nextInt();
+		while(in.hasNext()){
+			
+			
+			aux = 0;
+			cont = 0;
+			
+			
 		
-		HashA ha = new HashA(m);
-		HashB hb = new HashB(m);
-		HashC hc = new HashC(m);
-		
-		//quantidade de senhas a serem inseridas
-		for(int i = 0; i < n; ++i){
+			//----------------------------------
 			
-			//recebe o valor à ser adicionado nos respectivos hashs
-			aux = in.nextInt();
+			k = in.nextInt();
+			m = in.nextInt();
+			n = in.nextInt();
+			q = in.nextInt();
 			
-			//Adiciona nos hashs
-			ha.add(aux);
-			hb.add(aux);
-			hc.add(aux);
+			HashA ha = new HashA(m);
+			HashB hb = new HashB(m);
+			HashC hc = new HashC(m);
 			
-		}
-		
-		hb.order();
-		
-		//quantidade de consultas a serem realizadas
-		for(int i = 0; i < q; ++i){
-			
-			//recebe os valor à ser buscado
-			aux = in.nextInt();
-			
-			cont = ha.search(aux);
-			if(cont > 0){
-				foundA += cont;
-				cont = 0;
-			}else{
-				penetras++;
+			//quantidade de senhas a serem inseridas
+			for(int i = 0; i < n; ++i){
+				
+				//recebe o valor à ser adicionado nos respectivos hashs
+				aux = in.nextInt();
+				
+				//Adiciona nos hashs
+				ha.add(aux);
+				foundB += hb.add(aux);
+				hc.add(aux);
+				
 			}
 			
-			cont = hb.search(aux);
-			if(cont > 0){
-				foundB += cont;
-				cont = 0;
-			}
+			//hb.order();
 			
-			cont = hc.search(aux);
-			if(cont > 0){
-				foundC += cont;
-				cont = 0;
-			}
+			//quantidade de consultas a serem realizadas
+			for(int i = 0; i < q; ++i){
+				
+				//recebe os valor à ser buscado
+				aux = in.nextInt();
+				
+				cont = ha.search(aux);
+				if(cont > 0){
+					foundA += cont;
+					cont = 0;
+				}else{
+					penetras++;
+				}
+				
+				cont = hb.search(aux);
+				if(cont > 0){
+					foundB += cont;
+					cont = 0;
+				}
+				
+				cont = hc.search(aux);
+				if(cont > 0){
+					foundC += cont;
+					cont = 0;
+				}
 
 
+			}
+			
+			
+			System.out.println("caso "+contador+": "+penetras+" "+foundA+" "+foundB+" "+foundC);
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			contador++;
 		}
 		
 		
-		System.out.println("Penetras: "+penetras+"| a: "+foundA+" |b: "+foundB+" |c: "+foundC+" ");
 		
 		
 		
@@ -90,7 +119,7 @@ public class Main {
 
 class HashA{
 	
-	int size;
+	double size;
 	ArrayList<Integer> elements = new ArrayList<>();
 	int senhas;
 	
@@ -147,9 +176,14 @@ class HashB{
 		senhas = 0;
 	}
 	
-	public void add(int _senha){
-		this.elements.add(_senha);
+	public int add(int _senha){
+	
+		this.elements.add(_senha); 
 		this.senhas++;
+		
+		this.order();
+	
+		return elements.indexOf(_senha);
 	}
 	
 	public boolean carga(){
@@ -184,47 +218,59 @@ class HashB{
 
 class HashC{
 	
-	int size;
-	int[][] elements;
+	double size;
+	Integer[][] elements;
 	int senhas;
 	
 	public HashC(int _size){
 		this.size = _size;
 		senhas = 0;
-		elements = new int[_size][_size];
+		elements = new Integer[_size][_size];
 	}
 	
-	public void add(int _senha){
+	public void add(
+			int _senha){
 		if(carga()){
 			rehash();
 		}
 		
-		this.elements[h0(_senha, this.size)][h1(_senha, this.size)] = _senha;
+		if(this.elements[h0(_senha, (int)this.size)][0] == null){
+			this.elements[h0(_senha, (int)this.size)][0] = _senha;
+		}else{
+			this.elements[h0(_senha, (int)this.size)][h1(_senha, (int) this.size)] = _senha;	
+		}
+		
 		this.senhas++;
 		
 	}
 	
 	public int h0(int _senha, int _size){
-		return _senha % this.size;
+		return (int) (_senha % this.size);
 	}
 	
 	public int h1(int _senha, int _size){
-		return (2 * (_senha % (_size / 2))) + 1;
+		return ((2 * (_senha % (_size / 2))) + 1);
 	}
 	
 	public boolean carga(){
-		return this.senhas/this.size >= 0.75;
+		double carga = this.senhas/this.size; 
+		//System.out.println(carga);
+		return carga >= 0.75;
 	}
 	
 	public void rehash(){
 		
-		int[][] aux = new int[this.size * 2][this.size * 2];
+		Integer[][] aux = new Integer[(int) this.size * 2][(int) this.size * 2];
 		
 		for(int i = 0 ; i < this.size; ++i){
 			for(int j = 0; j < this.size; ++j){
 				
-				if(elements[i][j] != '\0'){
-					aux[h0(elements[i][j], this.size * 2)][h1(elements[i][j], this.size * 2)] = elements[i][j];
+				if(elements[i][j] != null){
+					if(aux[h0(elements[i][j], (int) this.size * 2)][0] == null){
+						aux[h0(elements[i][j], (int) this.size * 2)][0] = elements[i][j];
+					}else{
+						aux[h0(elements[i][j], (int) this.size * 2)][h1(elements[i][j], (int) this.size * 2)] = elements[i][j];						
+					}
 				}
 				
 			}
@@ -243,7 +289,7 @@ class HashC{
 		for(int i = 0 ; i < this.size; ++i){
 			for(int j = 0; j < this.size; ++j){
 				
-				if(this.elements[i][j] != '\0' && !found){
+				if(this.elements[i][j] != null && !found){
 					comparison++;
 					if(this.elements[i][j] == _search){
 						found = true;
@@ -253,7 +299,12 @@ class HashC{
 			}
 		}
 		
-		return comparison;
+		if(found){
+			return comparison;
+		}else{
+			return 0;
+		}
+		
 		
 	}
 
