992aef444c9a086f1b93d155593e924b85e0fd7e
lucaszsd <lzsd@cin.ufpe.br>
2017-09-19 12:35:20 -0300

    Add files via upload

---
+++ Lista 3/HuxleyCode.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+
+public class HuxleyCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
+class directory{
+	
+	char name;
+	directory parent;
+	ArrayList<Object> bst = new ArrayList();
+	
+	public directory(char _name,directory _parent){
+		this.name = _name;
+		this.parent = _parent;
+	}
+	
+	public void add(Object _append){
+		bst.add(_append);
+	}
+
+}
+
+
+class file{
+	
+	char name;
+	
+	public file(char _name){
+		this.name = _name;
+	}
+	
+}
+
+class filesystem{
+	
+	directory actualdirectory;
+	directory root;
+	
+	public filesystem() {
+		//root serve como referencia para a raiz do sistema de arquivos
+		root = new directory('/', null);
+		this.actualdirectory = root;
+	}
+	
+	
+	public void instruction(String _inst){
+		String inst = _inst.split(" ")[0];
+		String target = "";
+		
+		switch(inst){
+		
+			//entra no diretorio
+			case "cd":
+				target = _inst.split(" ")[1];
+				break;
+			//lista elementos do diretório
+			case "ls":
+				break;
+			//cria novo arquivo no diretorio atual
+			case "touch":
+				target = _inst.split(" ")[1];
+				break;
+			//cria diretório
+			case "mkdir":
+				target = _inst.split(" ")[1];
+				break;
+			//imprime nome do diretorio
+			case "pwd":
+				System.out.println(this.actualdirectory.name);
+				break;
+			//imprime a cadeia de diretorios conforme pedido
+			case "tree":
+				target = _inst.split(" ")[1];
+				break;
+			//deleta arquivo ou diretorio
+			case "rm":
+				target = _inst.split(" ")[1];
+				break;
+			default:
+				break;
+			
+		}
+		
+		
+		
+	}
+	
+}
